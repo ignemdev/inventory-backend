78fdad6987e5951a727ebcfcac2ca0b5a84fb313
Ignacio Morel P <ignaciomp.dev@gmail.com>
2022-03-11 08:28:06 -0400

    mappings

--- src/Inventory.Data/Extensions/ModelBuilderExtensions.cs
+++ src/Inventory.Data/Extensions/ModelBuilderExtensions.cs
@@ -26,4 +26,122 @@ public static class ModelBuilderExtensions
 
         return modelBuilder;
     }
+
+    public static ModelBuilder AddUnidadesMappings(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Unidad>(entity =>
+        {
+            entity.Property(u => u.Descripcion)
+               .IsRequired()
+               .HasMaxLength(DatabaseConstants.UnidadDescripcionMaxLength)
+               .IsUnicode(false);
+        }).AddBaseMappings();
+
+        return modelBuilder;
+    }
+
+    public static ModelBuilder AddRazonesMappings(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Razon>(entity =>
+        {
+            entity.Property(r => r.Descripcion)
+               .IsRequired()
+               .HasMaxLength(DatabaseConstants.RazonDescripcionMaxLength)
+               .IsUnicode(false);
+        }).AddBaseMappings();
+
+        return modelBuilder;
+    }
+
+    public static ModelBuilder AddProductosMappings(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.Property(p => p.Nombre)
+                .IsRequired()
+                .HasMaxLength(DatabaseConstants.ProductoNombreMaxLength)
+                .IsUnicode(false);
+
+            entity.Property(p => p.Descripcion)
+                .IsRequired()
+                .HasMaxLength(DatabaseConstants.ProductoDescripcionMaxLength)
+                .IsUnicode(false);
+
+            entity.Property(p => p.Stock)
+                .IsRequired();
+
+            entity.Property(p => p.Precio)
+                .IsRequired();
+
+            entity.HasOne(p => p.Unidad)
+                .WithMany()
+                .HasForeignKey(p => p.UnidadId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+
+        }).AddBaseMappings();
+
+        return modelBuilder;
+    }
+
+    public static ModelBuilder AddEntradasMappings(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Entrada>(entity =>
+        {
+            entity.Property(e => e.Cantidad)
+                .IsRequired();
+        
+            entity.HasOne(e => e.Producto)
+                .WithMany()
+                .HasForeignKey(e => e.ProductoId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        
+        }).AddBaseMappings();
+
+        return modelBuilder;
+    }
+
+    public static ModelBuilder AddSalidasMappings(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Salida>(entity =>
+        {
+            entity.Property(s => s.Cantidad)
+                .IsRequired();
+
+            entity.HasOne(s => s.Producto)
+                .WithMany()
+                .HasForeignKey(s => s.ProductoId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            entity.HasOne(s => s.Razon)
+                .WithMany()
+                .HasForeignKey(s => s.RazonId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }).AddBaseMappings();
+
+        return modelBuilder;
+    }
+
+    private static ModelBuilder AddBaseMappings(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BaseEntity>(entity =>
+        {
+            entity.HasKey(b => b.Id);
+
+            entity.HasOne(b => b.Usuario)
+                .WithMany()
+                .HasForeignKey(b => b.UsuarioId)
+                .IsRequired(false)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            entity.Property(b => b.Creado).HasColumnType("datetime");
+            entity.Property(b => b.Modificado).HasColumnType("datetime");
+        });
+
+        return modelBuilder;
+    }
 }
