085447059d6df6d2f3cb1da89327e75b59450f62
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-03-10 22:55:34 -0400

    clase startup creada

---
+++ src/Inventory.Api/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Inventory.Api;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration) => Configuration = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddCors();
+
+        services.AddControllers()
+                .AddJsonOptions(x =>
+                {
+                    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                    x.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                });
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddAutoMapper(typeof(Startup));
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseCors(x => x.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+    }
+}
