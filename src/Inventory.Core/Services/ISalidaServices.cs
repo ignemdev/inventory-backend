1351d767acfcf856674bd0322bae9b28a9489cb5
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-03-12 01:32:58 -0400

    servicios de salidas

---
+++ src/Inventory.Core/Services/ISalidaServices.cs
@@ -0,0 +1,15 @@
+﻿using Inventory.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Core.Services;
+
+public interface ISalidaServices
+{
+    Task<IEnumerable<Salida>> GetAllSalidas();
+    Task<Salida> GetSalidaById(int salidaId);
+    Task<Salida> AddSalida(Salida salida);
+}
---
+++ src/Inventory.Services/SalidaServices.cs
@@ -0,0 +1,59 @@
+﻿using Inventory.Core;
+using Inventory.Core.Entities;
+using Inventory.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Services;
+
+public class SalidaServices : ISalidaServices
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public SalidaServices(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    public async Task<Salida> AddSalida(Salida salida)
+    {
+        var errors = new List<ValidationResult>();
+        if (!Validator.TryValidateObject(salida, new ValidationContext(salida), errors, true))
+            throw new InvalidOperationException(string.Join(Environment.NewLine, errors.Select(x => x.ErrorMessage)));
+
+        if (salida == null)
+            throw new ArgumentNullException(Messages.E003);
+
+        var addedSalida = await _unitOfWork.Salidas.AddAsync(salida);
+        await _unitOfWork.SaveAsync();
+
+        await _unitOfWork.Productos.UpdateStockAsync(addedSalida.ProductoId, addedSalida.Cantidad * -1);
+
+        return addedSalida;
+    }
+
+    public async Task<IEnumerable<Salida>> GetAllSalidas()
+    {
+        var salidas = await _unitOfWork.Salidas.GetAllAsync(includeProperties: "Creador,Modificador,Producto",
+            orderBy: s => s.OrderByDescending(x => x.Creado));
+
+        return salidas;
+    }
+
+    public async Task<Salida> GetSalidaById(int salidaId)
+    {
+        if (salidaId == 0)
+            throw new ArgumentNullException(Messages.E004);
+
+        var dbSalida = await _unitOfWork.Salidas.GetFirstOrDefaultAsync(s => s.Id == salidaId,
+            includeProperties: "Creador,Modificador,producto");
+
+        if (dbSalida == null)
+            throw new NullReferenceException(Messages.E005);
+
+        return dbSalida;
+    }
+}
+
