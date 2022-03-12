d384419a06d52fc2ea33577f8581b3036fe8bb21
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-03-11 23:59:11 -0400

    repositorio de salida

---
+++ src/Inventory.Data/Repositories/SalidaRepository.cs
@@ -0,0 +1,16 @@
+﻿using Inventory.Core.Entities;
+using Inventory.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Data.Repositories;
+
+public class SalidaRepository : Repository<Salida>, ISalidaRepository
+{
+    private readonly InventoryContext _db;
+    public SalidaRepository(InventoryContext db) : base(db) => _db = db;
+}
