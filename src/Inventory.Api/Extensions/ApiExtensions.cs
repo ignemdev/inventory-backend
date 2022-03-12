2ee1b53fcd9eb1090eeeabd14e48bda3d6291986
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-03-12 18:35:18 -0400

    segmentacion de servicios para swagger y los controladores

---
+++ src/Inventory.Api/Extensions/ApiExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace System;
+
+public static class ApiExtensions
+{
+    public static IServiceCollection AddControllersConfigurations(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme."
+
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddSwaggerConfigurations(this IServiceCollection services)
+    {
+        services.AddControllers().AddJsonConfigurations();
+
+        return services;
+    }
+
+    private static IMvcBuilder AddJsonConfigurations(this IMvcBuilder mvcBuilder)
+    {
+        mvcBuilder.AddJsonOptions(x =>
+        {
+            x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            x.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        });
+
+        return mvcBuilder;
+    }
+}
--- src/Inventory.Api/Startup.cs
+++ src/Inventory.Api/Startup.cs
@@ -19,45 +19,15 @@ public class Startup
         services.AddDbContextConfiguration(Configuration);
         services.AddIdentityConfiguration();
         services.AddAuthenticationConfiguration(Configuration);
-        #endregion
 
         services.AddCors();
+
         services.AddHttpContextAccessor();
-        services.AddControllers()
-                .AddJsonOptions(x =>
-                {
-                    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-                    x.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-                });
 
-        services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen(c =>
-        {
-            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-            {
-                Name = "Authorization",
-                Type = SecuritySchemeType.ApiKey,
-                Scheme = "Bearer",
-                BearerFormat = "JWT",
-                In = ParameterLocation.Header,
-                Description = "JWT Authorization header using the Bearer scheme."
+        services.AddControllersConfigurations();    
+        services.AddSwaggerConfigurations();
+        #endregion
 
-            });
-            c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                          new OpenApiSecurityScheme
-                          {
-                              Reference = new OpenApiReference
-                              {
-                                  Type = ReferenceType.SecurityScheme,
-                                  Id = "Bearer"
-                              }
-                          },
-                         new string[] {}
-                    }
-                });
-        });
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddEntitiesServices();
         services.AddAutoMapper(typeof(Startup));
@@ -65,7 +35,6 @@ public class Startup
 
     public void Configure(WebApplication app, IWebHostEnvironment env)
     {
-
         if (env.IsDevelopment())
         {
             app.UseSwagger();
