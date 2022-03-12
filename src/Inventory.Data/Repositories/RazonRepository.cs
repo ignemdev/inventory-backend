8cc1bc78a3cdbd4e74079d66ef1308aef86a88b9
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-03-11 23:08:25 -0400

    repositorio de razon

---
+++ src/Inventory.Data/Repositories/RazonRepository.cs
@@ -0,0 +1,25 @@
+﻿using Inventory.Core.Entities;
+using Inventory.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Data.Repositories;
+
+public class RazonRepository : Repository<Razon>, IRazonRepository
+{
+    private readonly InventoryContext _db;
+    public RazonRepository(InventoryContext db) : base(db) => _db = db;
+    public async Task<Razon> UpdateAsync(Razon razon)
+    {
+        var dbRazon = await _db.Razones.FirstOrDefaultAsync(c => c.Id == razon.Id);
+
+        if (dbRazon != null)
+            dbRazon!.Descripcion = razon.Descripcion ?? dbRazon.Descripcion;
+
+        return dbRazon!;
+    }
+}
