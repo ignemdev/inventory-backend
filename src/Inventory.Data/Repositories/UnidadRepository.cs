f45298bdae65318ae2156e76f34eca12d8c5e2ff
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-03-11 23:05:53 -0400

    repositorio de unidad

---
+++ src/Inventory.Data/Repositories/UnidadRepository.cs
@@ -0,0 +1,25 @@
+﻿using Inventory.Core.Entities;
+using Inventory.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Data.Repositories;
+
+public class UnidadRepository : Repository<Unidad>, IUnidadRepository
+{
+    private readonly InventoryContext _db;
+    public UnidadRepository(InventoryContext db) : base(db) => _db = db;
+    public async Task<Unidad> UpdateAsync(Unidad unidad)
+    {
+        var dbUnidad = await _db.Unidades.FirstOrDefaultAsync(c => c.Id == unidad.Id);
+
+        if (dbUnidad != null)
+            dbUnidad!.Descripcion = unidad.Descripcion ?? dbUnidad.Descripcion;
+
+        return dbUnidad!;
+    }
+}
